f7bd7ffc3deb988dcada76e8c758b6a73f42163b
rodrigo5611 <rodrigonicolas7654@gmail.com>
2025-10-02 18:06:03 -0300

    Se agrego que se pueda guardar el nombre y la imagen de usuario en la base de datos

--- Presentacion/HTML/Ajustes.php
+++ Presentacion/HTML/Ajustes.php
@@ -14,7 +14,6 @@ include_once 'cabecera.php';
 ?>
 <body class="Inicio-Ajustes">
 
-
     <!--Menu de Ajustes-->
     <main id="Ajustes">
         <nav class="columna-Bajustes">
@@ -27,22 +26,43 @@ include_once 'cabecera.php';
         <!-- Sección para editar perfil -->
         <section id="perfil" style="display:none; margin-top:20px;">
             <div class="contenedor">
-            <h4>Editar Perfil</h4>
+                <h4>Editar Perfil</h4>
 
-            <p>Elige tu nueva imagen de perfil:</p>
-            <figure class="opciones-imagenes">
-                <img src="https://i.imgur.com/dI9Sv1L.png" class="opcion" alt="Imagen 1">
-                <img src="https://i.imgur.com/pWERaQp.png" class="opcion" alt="Imagen 2">
-                <img src="https://i.imgur.com/EFIGa8n.png" class="opcion" alt="Imagen 3">
-                <img src="https://i.imgur.com/vmu94wH.png" class="opcion" alt="Imagen 4">
-                <img src="https://i.imgur.com/QR48PuX.png" class="opcion" alt="Imagen 5">
-            </figure>
+                <!-- FORMULARIO -->
+                <form action="../../Negocio/ActualizarPerfil.php" method="POST">
+                    
+                    <p>Elige una imagen:</p>
+                    <figure class="opciones-imagenes">
+                        <label>
+                            <input type="radio" name="imagen" value="https://i.imgur.com/dI9Sv1L.png" required>
+                            <img src="https://i.imgur.com/dI9Sv1L.png" class="opcion" alt="Imagen 1">
+                        </label>
+                        <label>
+                            <input type="radio" name="imagen" value="https://i.imgur.com/pWERaQp.png">
+                            <img src="https://i.imgur.com/pWERaQp.png" class="opcion" alt="Imagen 2">
+                        </label>
+                        <label>
+                            <input type="radio" name="imagen" value="https://i.imgur.com/EFIGa8n.png">
+                            <img src="https://i.imgur.com/EFIGa8n.png" class="opcion" alt="Imagen 3">
+                        </label>
+                        <label>
+                            <input type="radio" name="imagen" value="https://i.imgur.com/vmu94wH.png">
+                            <img src="https://i.imgur.com/vmu94wH.png" class="opcion" alt="Imagen 4">
+                        </label>
+                        <label>
+                            <input type="radio" name="imagen" value="https://i.imgur.com/QR48PuX.png">
+                            <img src="https://i.imgur.com/QR48PuX.png" class="opcion" alt="Imagen 5">
+                        </label>
+                    </figure>
 
-            <p>Nuevo nombre:</p>
-            <input type="text" id="nuevoNombre" placeholder="Escribe tu nombre">
-            <br>
-            <button id="guardar">Guardar Cambios</button>
-            <br>
+                    <p>Nuevo nombre:</p>
+                    <input type="text" name="nuevoNombre" id="nuevoNombre" placeholder="Escribe tu nombre" required>
+                    <br><br>
+
+                    <button type="submit" id="guardar">Guardar Cambios</button>
+                </form>
+                <!-- FIN FORMULARIO -->
+
             </div>
         </section>
     </main>  
--- Proyecto_Teknova 6.0/Presentacion/HTML/autenticado.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once '../../Negocio/usuario.php';
-session_start();
-if (!isset($_SESSION["usuario_id"])){
- header(header: "Location: login.php");
- exit();
-}
-?>
