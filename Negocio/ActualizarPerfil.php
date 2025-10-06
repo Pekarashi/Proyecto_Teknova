feff28fad7f79cdfee13a4b753fa85f11f1283fa
rodrigo5611 <rodrigonicolas7654@gmail.com>
2025-10-06 18:15:16 -0300

    Se arreglo las imagenes de los dinosaurios y cuando cambie el nombre se cambie tambien el la base de datos.

--- Negocio/ActualizarPerfil.php
+++ Negocio/ActualizarPerfil.php
@@ -1,13 +1,46 @@
 <?php
-session_start();
+session_start(); 
+include_once '../Datos/conexion.php'; 
 
 if (isset($_POST['nuevoNombre']) && isset($_POST['imagen'])) {
-    $_SESSION['nombre'] = $_POST['nuevoNombre'];
-    $_SESSION['imagen_perfil'] = $_POST['imagen'];
+    // Verifica que el formulario haya mandado un nuevo nombre y una imagen.
+
+    $nuevoNombre = $_POST['nuevoNombre']; 
+    $imagen = $_POST['imagen'];           
+
+    $usuario_id = $_SESSION['usuario_id']; 
+    
+
+    // Prepara la consulta SQL para cambiar nombre e imagen.
+    $sql = "UPDATE usuario 
+            SET nombre = ?, imagen = ? 
+            WHERE usuario_id = ?";
+    
+    $stmt = $conexion->prepare($sql); 
+    // evitamos inyección SQL.
+
+    if (!$stmt) {
+        // Si algo falla al preparar la consulta, mostramos el error.
+        die("Error en la preparación: " . $conexion->error);
+    }
+
+    // se enlazamos los valores a la consulta: 
+    // "s" = string, "i" = entero.
+    $stmt->bind_param("ssi", $nuevoNombre, $imagen, $usuario_id);
+
+    if ($stmt->execute()) {
+        // Si la consulta se ejecuta bien, también actualizamos la sesión.
+        $_SESSION['nombre'] = $nuevoNombre;
+        $_SESSION['imagen_perfil'] = $imagen;
+    } else {
+        // si la consulta falla muetra error .
+        die("Error al ejecutar: " . $stmt->error);
+    }
 
     header("Location: ../Presentacion/HTML/Ajustes.php");
     exit;
 } else {
+    
     header("Location: ../Presentacion/HTML/Ajustes.php");
     exit;
 }
